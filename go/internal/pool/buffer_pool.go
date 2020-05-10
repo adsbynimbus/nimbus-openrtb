fdd6435216964b313620f372f468831c28c641c2
Marc Santiago <marcanthonysanti@gmail.com>
2020-05-10 18:21:10 -0400

    Pool: Adding an internal buffer pool for native

---
+++ go/internal/pool/buffer_pool.go
@@ -0,0 +1,25 @@
+package pool
+
+import (
+	"bytes"
+	"sync"
+)
+
+// bytesBuffer creates a Bytes.Buffer pool that can be used to stop object churn
+var bytesBuffer = sync.Pool{
+	// New is called when a new instance is needed
+	New: func() interface{} {
+		return new(bytes.Buffer)
+	},
+}
+
+// GetBuffer fetches a buffer from the pool
+func GetBuffer() *bytes.Buffer {
+	return bytesBuffer.Get().(*bytes.Buffer)
+}
+
+// PutBuffer returns a buffer to the pool, never call this with defer in loops
+func PutBuffer(buf *bytes.Buffer) {
+	buf.Reset()
+	bytesBuffer.Put(buf)
+}
