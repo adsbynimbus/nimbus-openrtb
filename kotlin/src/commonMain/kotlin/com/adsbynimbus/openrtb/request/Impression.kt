3f95a046a72d056eaa71f56d6798962a6a1e4686
Jason Sznol <jason@timehop.com>
2022-02-02 14:46:43 -0500

    Updated APS params to use Collection type

--- kotlin/src/commonMain/kotlin/com/adsbynimbus/openrtb/request/Impression.kt
+++ kotlin/src/commonMain/kotlin/com/adsbynimbus/openrtb/request/Impression.kt
@@ -55,7 +55,7 @@ public class Impression(
     @Serializable
     public class Extension(
         @JvmField @SerialName("position") public var position: String,
-        @JvmField @SerialName("aps") public var aps: Array<Map<String, List<String>>> = emptyArray(),
+        @JvmField @SerialName("aps") public var aps: Collection<Map<String, List<String>>> = emptyList(),
         @JvmField @SerialName("facebook_app_id") public var facebook_app_id: String = "",
         @JvmField @SerialName("facebook_test_ad_type") public var facebook_test_ad_type: String = "",
     )
