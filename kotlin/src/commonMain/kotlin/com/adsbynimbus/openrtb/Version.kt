1767736a4b2ea43d97c9cc30421382ddc8caf459
Jason Sznol <jason@timehop.com>
2022-02-02 08:50:04 -0500

    Updated header constant names for readability in Kotlin implementations

--- kotlin/src/commonMain/kotlin/com/adsbynimbus/openrtb/Version.kt
+++ kotlin/src/commonMain/kotlin/com/adsbynimbus/openrtb/Version.kt
@@ -7,7 +7,7 @@ import kotlin.jvm.JvmMultifileClass
 import kotlin.jvm.JvmName
 
 /** Required header for all requests to Nimbus defining the OpenRTB version */
-public const val VERSION_HEADER: String = "x-openrtb-version"
+public const val OPENRTB_HEADER: String = "x-openrtb-version"
 
 /** The current supported OpenRTB version by this request object */
-public const val VERSION: String = "2.5"
+public const val OPENRTB_VERSION: String = "2.5"
