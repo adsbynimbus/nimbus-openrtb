075d83630c4fb75d7a9e07a8d0e9b8dddd75dc19
Jason Sznol <jason@nimbus.co>
2024-11-26 11:41:01 -0500

    Added content filters to the Google repositories.
    Add Android Gradle Plugin override functionality for Android Studio Canary usage

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,16 +2,35 @@
 
 pluginManagement {
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
+val androidGradleOverride = with(providers) {
+    gradleProperty("android.studio.version").flatMap { gradleProperty("android.gradle") }
+}
+
 dependencyResolutionManagement {
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
+    }
+    versionCatalogs.configureEach {
+          if (androidGradleOverride.isPresent) version("android", androidGradleOverride.get())
     }
 }
 
