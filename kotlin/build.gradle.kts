22c047465c76e8a1cdf2b9f8a73eb3ad615254f1
Jason Sznol <jason@nimbus.co>
2025-05-02 14:27:44 -0400

    Updated Android compileSdk to 36

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         minSdk = 21
         consumerProguardFile("src/androidMain/consumer-proguard-rules.pro")
