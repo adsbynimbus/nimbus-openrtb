0a6cc73c8c4ad1e5bc8f3f96bd73a8de3f0f288c
Jason Sznol <jason@nimbus.co>
2025-05-02 10:02:45 -0400

    Added integration test for Kotlin 1.9, Android Gradle 8.2.2, and Serialization 1.5.1

---
+++ integration-tests/android/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.android)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    //noinspection GradleDependency
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+    }
+    namespace = "com.adsbynimbus.openrtb.android"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    sourceSets.configureEach {
+        if (name.contains("test", ignoreCase = true)) {
+            java.srcDirs("../../kotlin/src/commonTest/kotlin")
+        }
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+dependencies {
+    implementation(libs.openrtb)
+    implementation(libs.serialization.json)
+    implementation(libs.bundles.test)
+}
---
+++ integration-tests/settings.gradle.kts
@@ -0,0 +1,33 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenLocal()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+rootProject.name = "nimbus-openrtb-integration-tests"
+
+include("android")
