794e0991eb1116dd76eb48486d07962d564db929
Renam Philippi Antunes <renam@nimbus.co>
2025-06-04 13:57:32 -0300

    Ensure support for kotlin 1.8 (#167)
    * sets integration test to run against 1.8
    sets kotlin api to 1.8
    
    * sets kotlin language to 1.8

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.kotlin.gradle.dsl.*
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
     alias(libs.plugins.android)
@@ -37,8 +38,8 @@ kotlin {
     sourceSets {
         configureEach {
             languageSettings {
-                apiVersion = KotlinVersion.KOTLIN_1_9.version
-                languageVersion = KotlinVersion.KOTLIN_1_9.version
+                apiVersion = KotlinVersion.KOTLIN_1_8.version
+                languageVersion = KotlinVersion.KOTLIN_1_8.version
                 optIn("kotlinx.serialization.ExperimentalSerializationApi")
             }
         }
